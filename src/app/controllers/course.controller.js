f39a96c6653437ca4f6dbb04bf1143119801c8f0
Đài <dinhdaistudio113@gmail.com>
2023-09-12 22:34:02 +0700

    Dài update add course và delete

--- src/app/controllers/course.controller.js
+++ src/app/controllers/course.controller.js
@@ -30,28 +30,29 @@ export const getCourseById = async (req, res) => {
     }
 };
 export const updateCourse = async (req, res) => {
-    try {
-        const record = await CourseModel.findByPk(req.params.id);
-        if (!record) {
-            res.status(404).json({ error: 'Record not found' });
-        } else {
-            await record.update(req.body);
-            res.status(200).json(record);
-        }
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+  try {
+      const record = await CourseModel.findByPk(req.params.id);
+      if (!record) {
+          res.status(404).json({ error: 'Record not found' });
+      } else {
+          await record.update(req.body);
+          res.status(200).json({ message: "Course updated successfully", data: record });  // Added success message
+      }
+  } catch (error) {
+      res.status(400).json({ error: error.message });
+  }
 };
+
 export const deleteCourse = async (req, res) => {
-    try {
-        const record = await CourseModel.findByPk(req.params.id);
-        if (!record) {
-            res.status(404).json({ error: 'Record not found' });
-        } else {
-            await record.destroy();
-            res.status(204).send();
-        }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  try {
+      const record = await CourseModel.findByPk(req.params.id);
+      if (!record) {
+          res.status(404).json({ error: 'Record not found' });
+      } else {
+          await record.destroy();
+          res.status(200).json({ message: 'Course deleted successfully!' }); 
+      }
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
 };
--- src/routes/course.route.js
+++ src/routes/course.route.js
@@ -3,6 +3,9 @@ let router = Router();
 import * as initCourse from "../app/controllers/course.controller"
 export default function initCourseRoute(app) {
     router.get("/", initCourse.getAllCourse)
+    router.get("/:id", initCourse.getCourseById)
     router.post("/", initCourse.createCourse)
+    router.delete("/:id", initCourse.deleteCourse)
+    router.put("/:id", initCourse.updateCourse)
     app.use("/course", router);
 }
