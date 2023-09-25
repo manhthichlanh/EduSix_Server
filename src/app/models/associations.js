c6a108719c10f85cf27010514821f913b153a6d2
Đài <dinhdaistudio113@gmail.com>
2023-09-25 22:58:11 +0700

    update category CRUD,relationship & course

--- src/app/models/associations.js
+++ src/app/models/associations.js
@@ -1,8 +1,13 @@
 import CourseModel from './course.model';
 import SectionModel from './section.model';
 import LessonModel from './lesson.model';
+import CategoryModel from './category.model';
 // Định nghĩa mối quan hệ giữa Course và Section
 CourseModel.hasMany(SectionModel, { foreignKey: 'course_id' });
 SectionModel.belongsTo(CourseModel, { foreignKey: 'course_id' });
+// Định nghĩa mối quan hệ giữa Section và Lesson
 SectionModel.hasMany(LessonModel, { foreignKey: 'section_id' });
 LessonModel.belongsTo(SectionModel, { foreignKey: 'section_id' });
+// Định nghĩa mối quan hệ giữa Category và Course
+CategoryModel.hasMany(CourseModel, { foreignKey: 'category_id'});
+CourseModel.hasMany(CategoryModel, { foreignKey: 'category_id'});
