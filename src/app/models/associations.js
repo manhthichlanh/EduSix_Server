54a5439675adbdd03548c72bbfa3cec56b48e896
Đài <dinhdaistudio113@gmail.com>
2023-09-25 23:20:01 +0700

    update relationship lesson và videos

--- src/app/models/associations.js
+++ src/app/models/associations.js
@@ -2,6 +2,7 @@ import CourseModel from './course.model';
 import SectionModel from './section.model';
 import LessonModel from './lesson.model';
 import CategoryModel from './category.model';
+import videoModel from './video.model';
 // Định nghĩa mối quan hệ giữa Course và Section
 CourseModel.hasMany(SectionModel, { foreignKey: 'course_id' });
 SectionModel.belongsTo(CourseModel, { foreignKey: 'course_id' });
@@ -11,3 +12,6 @@ LessonModel.belongsTo(SectionModel, { foreignKey: 'section_id' });
 // Định nghĩa mối quan hệ giữa Category và Course
 CategoryModel.hasMany(CourseModel, { foreignKey: 'category_id'});
 CourseModel.hasMany(CategoryModel, { foreignKey: 'category_id'});
+//Đình nghĩa mối quan hệ Lesson và videos
+LessonModel.hasMany(videoModel, { foreignKey: "lesson_id"});
+videoModel.belongsTo(LessonModel, { foreignKey: "lesson_id"});
