764b2df4bc7d46f9ce92e5b43153b4340413b5eb
Manh Nguyen <=>
2023-11-05 01:13:17 +0700

    Mạnh sửa duaration

--- src/app/controllers/admin-query.controller.js
+++ src/app/controllers/admin-query.controller.js
@@ -20,7 +20,7 @@ export const createLessonWithVideo = async (req, res, next) => {
                 section_id, name, content, type: lesson_type, duration, ordinal_number
             }, { transaction: t });
             //Cập nhật trường thứ tự (ordinal_number = lesson_id) của lesson vừa tạo
-            await newLesson.update({ ordinal_number: newLesson.lesson_id }, { fields: ['ordinal_number'], transaction: t });
+            await newLesson.update({ ordinal_number: newLesson.lesson_id, duration }, { fields: ['ordinal_number',['duration']], transaction: t });
             //Tạo video mới 
             const newVideo = await VideoModel.create({
                 lesson_id: newLesson.lesson_id,
